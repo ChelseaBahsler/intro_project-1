b311fe1ab0f564f93f8f99ab4d90390371ef7dd3
Foo Tang <godfoo@gmail.com>
2023-10-09 15:46:22 -0500

    removed the weakness controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  get 'card/index'
-  get 'card/show'
-  get 'locations/index'
   root "cards#index"
 
   resources :cards
   resources :supertypes
   resources :types
   resources :subtypes
-  resources :weaknesses
   resources :cardsets
+  resources :locations
 end
