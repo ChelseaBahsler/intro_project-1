f4aa61674512960b6da6e3d643fa522f9222a50a
Chelsea Bahsler <chelseabahsler@gmail.com>
2023-10-15 17:25:35 -0500

    Successfully implemented multi-model data on member pages

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,5 +5,8 @@ class Card < ApplicationRecord
   has_many :card_types
   has_many :types, through: :card_types
 
+  has_many :card_subtypes
+  has_many :subtypes, through: :card_subtypes
+
   validates :name, :artist, :imageurl, presence: true
 end
