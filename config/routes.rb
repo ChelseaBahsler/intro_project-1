04652d3c66633ce146f3be2bc4e8f247534d5eb4
Foo Tang <godfoo@gmail.com>
2023-10-17 19:05:59 -0500

    Changed back routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get "pages/about"
   root "cards#index"
   resources :cards, only: %i[index show]
-  resources :supertypes, only: %i[index]
-  resources :types, only: %i[index]
-  resources :subtypes, only: %i[index]
+  resources :supertypes, only: %i[index show]
+  resources :types, only: %i[index show]
+  resources :subtypes, only: %i[index show]
   resources :cardsets, only: %i[index show]
 end
