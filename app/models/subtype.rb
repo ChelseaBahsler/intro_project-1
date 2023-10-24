e091dafa51b45e0b9f1cfb074e4eb0195d7bfe82
Foo Tang <godfoo@gmail.com>
2023-10-24 11:38:51 -0500

    Fixed the subtype and type filters

--- app/models/subtype.rb
+++ app/models/subtype.rb
@@ -1,6 +1,6 @@
 class Subtype < ApplicationRecord
-  has_many :cardsubtypes
-  has_many :cards, through: :cardsubtypes
+  has_many :card_subtypes
+  has_many :cards, through: :card_subtypes
 
   validates :name, presence: true, uniqueness: true
 end
